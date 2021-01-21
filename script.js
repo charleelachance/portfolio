0bea41bd834de2849a847668cc7758f46cb38083
Charlee Lachance <m_c_lachance@yahoo.com>
2021-01-21 18:49:59 +0000

    Css for artwork/photography/coding pages

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// dropdown menu
+
 const openMenu = document.getElementById('open-menu')
 const closeMenu = document.getElementById('close-menu')
 const dropMenu = document.getElementById('drop-down')
@@ -13,3 +15,34 @@ closeMenu.addEventListener('click', function() {
   closeMenu.style.display = 'none'
   dropMenu.style.display = 'none'
 })
+
+
+// gallery
+
+// get the large image div
+const largeImage = document.getElementById('large-img')
+
+// get all of the smaller images and place in a list
+const imagesList = document.getElementsByClassName('image')
+
+const imageText = document.getElementById('main-txt')
+
+// loop through the small images list
+for (const element of imagesList){
+
+  // make the images "clickable"
+  element.addEventListener('click', function() {
+
+    // get the file location, width, and height from the image chosen
+    let image = element.getAttribute('src')
+    let text = element.getAttribute('alt')
+    let width = element.getAttribute('width')
+    // let height = element.getAttribute('height')
+
+    // set the large image div bg-image url, width, and height using the image variable
+    largeImage.setAttribute('src', image)
+    largeImage.setAttribute('width', width)
+    imageText.textContent = text
+    // largeImage.setAttribute('height', height)
+  })
+}
